d356870168254f3394a9ea160cb9ebf0d15f8ced
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-03-05 10:17:39 -0300

    criando contexto

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,13 +1,18 @@
+import { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { AuthContext } from '../contexts/auth';
 
 export default function RouteWrapper({
     component: Component,
     isPrivate,
     ...rest
 }) {
+    const { signed, loading } = useContext(AuthContext);
+
+
     // criar um sistema de login falso enquanto nao tem de fato
-    const loading = false;
-    const signed = false;
+    //const loading = false;
+    //const signed = false;
 
     if (loading) {
         <div>
