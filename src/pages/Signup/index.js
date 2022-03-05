86202359a69f1a67e38585dfb836f3ed74cdeed8
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-03-05 09:40:23 -0300

    criando tela de cadastro

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,19 +1,67 @@
+import { useState } from 'react';
+import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom';
 
+import {
+  Container,
+  AreaLogin,
+  AreaLogo,
+  AreaFormulario,
+  Botao,
+  Imagem,
+  Titulo,
+  Input,
+} from './styles';
 
 export default function Signup() {
-    return (
-      <div>
-        <h1 style={{ color: 'white' }}>oi, Raphael! você está na página de cadastro</h1>
-        <div style={{
-          backgroundColor: '#29292E',
-          height: 100,
-          width: 100,
-          borderRadius: 10,
-        }}>
-  
-        </div>
-      </div>
-    );
+
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [nome, setNome] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert('clicou')
   }
-  
-  
+
+  return (
+    <Container>
+      <AreaLogin>
+        <AreaLogo>
+          <Imagem
+            src={logo} alt='Sistema Logo'
+          />
+        </AreaLogo>
+
+        <AreaFormulario onSubmit={handleSubmit}>
+          <Titulo>Cadastrar uma conta</Titulo>
+          <Input
+            type="text"
+            placeholder='seu nome'
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          />
+          <Input
+            type="text"
+            placeholder='email@email.com'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            type="password"
+            placeholder='*******'
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+          <Botao type="submit">Cadastrar</Botao>
+        </AreaFormulario>
+
+        <Link style={{ margin: 20, color: '#000', cursor: 'pointer' }}
+          to='/'>Já tenho uma conta
+        </Link>
+
+      </AreaLogin>
+    </Container >
+  );
+}
+
--- src/pages/Signup/styles.js
+++ src/pages/Signup/styles.js
@@ -1,3 +1,69 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+height: 100%;
+display:flex;
+align-items: center;
+justify-content: center;
+background-color: #121212;
+`;
+
+export const AreaLogin = styled.div`
+background-color: #eaeaec;
+width: 600px;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+border-radius: 15px;
+`;
+
+export const AreaLogo = styled.div`
+display: flex;
+justify-content: center;
+background-color: #181c2e;
+width: 100%;
+border-top-right-radius: 10px;
+border-top-left-radius: 10px;
+`;
+
+export const Imagem = styled.img`
+padding: 20px;
+width: 170px;
+height: 130px;
+
+`;
+
+export const Titulo = styled.h1`
+text-align: center;
+color: #181c2e;
+margin-bottom: 0.5em;
+`;
+
+export const AreaFormulario = styled.div`
+margin-top: 1.5em;
+width: 90%;
+display: flex;
+flex-direction: column;
+`;
+
+export const Input = styled.input`
+margin-bottom: 15px;
+height: 35px;
+border: 0;
+border-radius: 7px;
+padding: 10px;
+font-size: 15px;
+background-color: #fff;
+`;
+
+export const Botao = styled.button`
+height: 35px;
+border:0;
+border-radius: 7px;
+background-color: #181c2e;
+color: #fff;
+font-weight: bold;
+font-size: 18px;
+`;
+
