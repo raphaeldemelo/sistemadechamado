9a41bd960c388a4a145f8fa6feaab25ecfb5440a
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-03-08 17:40:45 -0300

    apagando codigo desnecessário

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { useContext } from 'react';
-import { AuthContext } from '../../contexts/auth';
 
 import {
     Container,
     Texto,
-    BotaoSair,
 } from './styles';
 import Header from '../../components/Header';
 
 export default function Dashboard() {
-
-    const { signOut } = useContext(AuthContext)
 
     return (
         <Container>
