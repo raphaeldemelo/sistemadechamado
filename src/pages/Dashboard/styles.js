020b7f32c82a732f9d6395a41f3c76862957b9b0
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-03-09 12:41:17 -0300

    refinando css do dashboard

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -9,6 +9,12 @@ export const Conteudo = styled.div`
 `;
 
 export const AreaDashboard = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+background-color: #292929;
+border-radius: 5px;
 
 @media screen and (min-width: 720px){
 margin-left: 310px;
@@ -16,7 +22,8 @@ margin-left: 310px;
 `;
 
 export const Texto = styled.h1`
-font-size: 12px;
+font-size: 1.3em;
+margin: 2em 0;
 `;
 
 export const Botao = styled.button`
@@ -27,4 +34,5 @@ padding: 10px;
 align-items: center;
 justify-content: center;
 display: flex;
+margin-bottom: 20px;
 `;
